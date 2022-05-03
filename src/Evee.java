57a201402de6838e0a4d5dc0c3366c08937d1542
Aryan Chavan <aryannc980@gmail.com>
2022-05-03 19:45:02 -0400

    2 new pokemons added

---
+++ src/Evee.java
@@ -0,0 +1,31 @@
+public class Evee implements Health, Type, Attacks{
+    @Override
+    public String attack1() {
+        return null;
+    }
+
+    @Override
+    public String attack2() {
+        return null;
+    }
+
+    @Override
+    public String attack3() {
+        return null;
+    }
+
+    @Override
+    public String attack4() {
+        return null;
+    }
+
+    @Override
+    public int initHP() {
+        return 0;
+    }
+
+    @Override
+    public String pokemonType() {
+        return null;
+    }
+}
