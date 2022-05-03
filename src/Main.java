75154c228927b86ba056ea66f3455ab6de3dffe1
Aryan Chavan <aryannc980@gmail.com>
2022-05-03 09:27:59 -0400

    stats Commit

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,13 @@ public class Main {
         Bulbasaur bulbasaur = new Bulbasaur("Bulbasaur");
 
         String [] pikachuStats = {"Type: " + pikachu.pokemonType(), "Attacks: " + pikachu.attack1(), pikachu.attack2(), pikachu.attack3(), pikachu.attack4()};
+        String [] charmaderStats = {"Type: " + charmander.pokemonType(), "Attacks: " + charmander.attack1(), charmander.attack2(), charmander.attack3(), charmander.attack4()};
+        String [] squirtleStats = {"Type: " + squirtle.pokemonType(), "Attacks: " + squirtle.attack1(), squirtle.attack2(), squirtle.attack3(), squirtle.attack4()};
+        String [] bulbasaurStats = {"Type: " + bulbasaur.pokemonType(), "Attacks: " + bulbasaur.attack1(), bulbasaur.attack2(), bulbasaur.attack3(), bulbasaur.attack4()};
+
         System.out.println("Pikachu:  " + Arrays.toString(pikachuStats));
+        System.out.println("Charmander:  " + Arrays.toString(charmaderStats));
+        System.out.println("Squirtle:  " + Arrays.toString(squirtleStats));
+        System.out.println("Bulbasaur:  " + Arrays.toString(bulbasaurStats));
     }
 }
